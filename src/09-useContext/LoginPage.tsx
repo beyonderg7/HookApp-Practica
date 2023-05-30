688d2b8c6c5067a54c129651da61dcd74e41cfe7
Christian <christianyabeta@gmail.com>
2023-05-30 15:35:45 -0400

    Providers y Context

--- src/09-useContext/LoginPage.tsx
+++ src/09-useContext/LoginPage.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+ import  {useContext} from 'react'
+import { UserContext } from './context/UserContext';
 
 export const LoginPage = () => {
+
+     const {user, setUser} =  useContext(UserContext);
+
+    // console.log(user);
+
+
   return (
     
     <>
         <h1>LoginPage</h1>
         <hr></hr>
+
+        
+        <pre>
+          {    JSON.stringify(user, null, 3)  }
+        </pre>
+
+        <button 
+          className='btn btn-primary ms-2'
+          onClick={() => setUser(
+              {
+              id: 7,
+              name: 'Vegeta',
+              email: 'yoSoyunGuerreroSayayin!@gmail.com'
+              }
+          )}
+          >
+
+          Activar 
+        </button>
     </>
   )
 }
--- src/09-useContext/Navbar.tsx
+++ src/09-useContext/Navbar.tsx
@@ -27,10 +27,6 @@ export const Navbar:FC = () => {
             <NavLink to={'/login'} className={(props) => `nav-link ${props.isActive ? 'active' : '' }`}>Login</NavLink>
           </li>
         </ul>
-        <form className="d-flex" role="search">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-          <button className="btn btn-outline-success" type="submit">Search</button>
-        </form>
       </div>
     </div>
   </nav>
